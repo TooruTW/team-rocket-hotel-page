bdd579450c9e64acdf72a42c34bc8fe0cc27870d
Tooru Wu <tooruwu@ToorudeMacBook-Air.local>
2025-04-10 14:59:06 +0800

    feat-mobile-layout

--- src/components/Section5.vue
+++ src/components/Section5.vue
@@ -8,12 +8,13 @@
 
 </script>
 <template>
-    <div class="h-screen w-full bg-theme-primary-10 relative">
+    <div class="min-h-screen w-full flex items-center justify-end bg-theme-primary-10 relative ">
         <!-- decoration -->
-        <img class="w-50 absolute -top-10 right-20" src="/public/dots.png" alt="decroation-dot">
-        <div class="w-full h-full flex items-center ml-10 justify-between">
-            <img class="h-screen mt-13.5" src="/public/wave-line-vertical.png" alt="decroation-wave-vertical">
-            <div class="w-[83.75%] flex flex-col justify-between h-full py-30">
+        <img class="w-50 absolute -top-10 right-20 max-xl:hidden" src="/public/dots.png" alt="decroation-dot">
+        <img class="h-screen absolute top-13.5 left-10 max-xl:hidden" src="/public/wave-line-vertical.png" alt="decroation-wave-vertical">
+
+            <div class="w-[80%] h-200 flex flex-col justify-between gap-10 my-20
+                        max-xl:w-full max-xl:px-3 max-xl:h-180">
                 <!-- title -->
                 <div class="flex items-center gap-10 leading-[1.2] tracking-wider">
                     <h1 class="h-full text-theme-primary-100 text-48 font-bold leading-[1.2] tracking-wider max-md:text-32">
@@ -22,27 +23,26 @@
                     <div class=" w-35 h-0.5 bg-linear-to-r from-theme-primary-100 to-theme-neutral-0"></div>
                 </div>
                 <!-- cardcontainer -->
-                    <div class="w-full h-150 flex gap-6 overflow-x-auto">
-                        <!-- card -->
-                        <div    v-for="(culiary,index) in culiaryArr"
-                                class="h-full w-104 border-1 rounded-md flex flex-col justify-end flex-shrink-0
-                                  text-theme-neutral-0 bg-center bg-cover"
+                <div class="w-full h-full flex gap-6 overflow-x-scroll snap-x">
+                    <!-- card -->
+                    <div    v-for="(culiary,index) in culiaryArr"
+                                class="h-full w-1/3 max-2xl:w-1/2 max-md:w-full 
+                                    border-1 rounded-md flex flex-col justify-end flex-shrink-0
+                                  text-theme-neutral-0 bg-center bg-cover snap-center"
                                 :style="{backgroundImage: `url(${culiary.image})`}">
                             <!-- textcontent --> 
                             <div class="h-1/3 rounded-b-md p-6 flex flex-col gap-6
                                         backdrop-blur-xl bg-linear-to-b from-[#000000 0%] to-theme-neutral-bg">
                                 <div class="flex justify-between">
                                     <h3 class="leading-[1.2] tracking-wider text-24 font-bold">{{ culiary.title }}</h3>
-                                    <h4 class="leading-[1.5] tracking-tight text-base font-bold">{{ culiary.diningTime }}</h4>
+                                    <h4 class="leading-[1.5] tracking-tight text-base max-md:text-14 font-bold">{{ culiary.diningTime }}</h4>
                                 </div>
-                                <p class="leading-[1.5] tracking-tight text-base font-medium">{{ culiary.description }}</p>
+                                <p class="leading-[1.5] tracking-tight text-base max-md:text-14  font-medium">{{ culiary.description }}</p>
                             </div>
-                        </div>
                     </div>
+                </div>
 
             </div>
-
-        </div>
 
     </div>
 </template>
