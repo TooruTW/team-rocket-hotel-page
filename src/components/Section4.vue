0d6458640882058f03c9956bce7d0704a5ef681a
Tooru Wu <tooruwu@ToorudeMacBook-Air.local>
2025-04-10 09:49:37 +0800

    update-remove-console.log

--- src/components/Section4.vue
+++ src/components/Section4.vue
@@ -5,21 +5,15 @@ import { getData } from '../apiFunction';
 import RectWideBtn from './RectWideBtn.vue';
 
 const token = inject("token")
-
 const roomInfoUrl = "https://team-rocket-hotelapi-from-freyja.onrender.com/api/v1/admin/rooms/"
-
 const roomInfoArr = await getData(roomInfoUrl,token)
-
 const currentRoom = ref(0)
-
 const currentRoomImageList = computed(()=>roomInfoArr[currentRoom.value].imageUrlList)
-
 const roomType = computed(() => roomInfoArr[currentRoom.value].name)
 const roomDescribe = computed(() => roomInfoArr[currentRoom.value].description)
 const roomPrice = computed(() => roomInfoArr[currentRoom.value].price)
 
 function handleArrow(diraction){
-
     const isRight = diraction;
     const isLastRoom = (roomInfoArr.length - 1 ) === currentRoom.value ? true : false ;
     const isFirstRoom = currentRoom.value === 0 ? true : false ;
@@ -28,46 +22,32 @@ function handleArrow(diraction){
     }else{
         currentRoom.value = isFirstRoom ? currentRoom.value = (roomInfoArr.length -1) : currentRoom.value - 1 ;
     }
-    console.log("clicked" , currentRoom.value)
-
 }
-
 
 </script>
 <template>
     <div class="w-full h-screen flex gap-20 items-end pb-30 bg-theme-neutral-bg relative overflow-hidden">
-
-        <img src="/wave-lines-horizone.png" class="w-[63%] object-contain absolute right-0 top-45 z-10" alt="theme-decoration-wave">
-        <img src="/public/decoration-1.png" class="w-full object-cover absolute top-[50%] " alt="decoration">
-
+            <img src="/wave-lines-horizone.png" class="w-[63%] object-contain absolute right-0 top-45 z-10" alt="theme-decoration-wave">
+            <img src="/public/decoration-1.png" class="w-full object-cover absolute top-[50%] " alt="decoration">
         <div class="w-[46%]">
             <Slider class="bg-center bg-cover aspect-square" :dataArr="currentRoomImageList"></Slider>
         </div>
-
         <div class="w-150 flex flex-col gap-10 text-theme-neutral-0 z-20">
             <div>
                 <h2 class="text-40 font-bold leading-[1.2] tracking-wider">{{ roomType }}</h2>
                 <p class="text-base font-medium leading-[1.5] tracking-wide">{{ roomDescribe }}</p>
             </div>
-
             <h3 class="text-32 font-bold leading-[1.2] tracking-wider">NT$ <span>{{ roomPrice }}</span></h3>
-
-           <RectWideBtn class="w-full" content="查看更多"></RectWideBtn>
-
+            <RectWideBtn class="w-full" content="查看更多"></RectWideBtn>
             <div class="text-theme-primary-100 flex items-center self-end">
-                <div @click="handleArrow(false)">
-                    <svg  class="m-3" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <svg @click="handleArrow(false)" class="m-3 hover:scale-150 active:scale-95 transition-all ease-in-out duration-250" width="24" height="24" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                     <path d="M12 20L13.41 18.59L7.83 13L20 13L20 11L7.83 11L13.41 5.41L12 4L4 12L12 20Z" fill="currentColor"/>
-                    </svg>
-                </div>
-                
-                <svg @click="handleArrow(true)" class="m-3" width="16" height="16" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                </svg>
+                <svg @click="handleArrow(true)" class="m-3 hover:scale-150 active:scale-95 transition-all ease-in-out duration-250" width="16" height="16" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                     <path d="M8 0L6.59 1.41L12.17 7H0V9H12.17L6.59 14.59L8 16L16 8L8 0Z" fill="currentColor"/>
                 </svg>
-
             </div>
         </div>
-
     </div>
 
 </template>
