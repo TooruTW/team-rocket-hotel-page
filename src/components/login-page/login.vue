96f6e3123913ebb7b9759e62705658609092908b
Tooru Wu <tooruwu@ToorudeMacBook-Air.local>
2025-04-11 10:55:49 +0800

    feat-login-form

--- src/components/login-page/login.vue
+++ src/components/login-page/login.vue
@@ -2,32 +2,39 @@
 
 </script>
 <template>
-    <div>
-        <nav></nav>
-        <div>
-            <img src="" alt="login-img">
-            <div>
-                <img src="" alt="decroation-wave">
-                <form action="">
+    <div class="w-full h-screen bg-theme-neutral-bg ">
+        <nav class="w-full h-120/1080 flex items-center">
+            <img class="w-48 ml-20" src="/public/hotel-icon.png" alt="hotel-icon">
+        </nav>
+        <div class="w-full h-960/1080 flex text-theme-neutral-0 ">
+            <img class="w-1/2 object-cover object-center" src="https://images.unsplash.com/photo-1630587148265-761cbd139043?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="login-img">
+            <div class="w-1/2 flex flex-col justify-center items-center relative">
+                <img class="w-304 absolute top-48 z-0" src="/public/wave-lines-horizone.png" alt="decroation-wave">
+                <form class="z-10 w-416/960 flex flex-col gap-10 " action="">
                     <div>
-                        <p></p>
-                        <h1></h1>
+                        <p class=" text-theme-primary-100 font-bold text-base leading-[1.5] tracking-wide">享樂酒店，誠摯歡迎</p>
+                        <h1 class="font-bold text-48 leading-[1.2] tracking-wider">立即開始旅程</h1>
                     </div>
-                    <label for="">
-                        <h5></h5>
-                        <input type="text">
-                    </label>
-                    <label for="">
-                        <h5></h5>
-                        <input type="text">
-                    </label>
-                    <label for="">
-                        <input type="checkbox">
-                        <a href=""></a>
-                    </label>
-                    <button></button>
-                    <h4>
-                        <a href=""></a>
+                    <div class="flex flex-col gap-2">
+                        <label class=" font-bold text-base leading-[1.5] tracking-wide w-full flex flex-col gap-2" for="">
+                            電子信箱
+                            <input class="w-full rounded-md p-4 bg-theme-neutral-40 text-theme-neutral-60" type="text" placeholder="hello@exsample.com">
+                        </label>
+                        <label class=" font-bold text-base leading-[1.5] tracking-wide w-full flex flex-col gap-2" for="">
+                            密碼
+                            <input class="w-full rounded-md p-4 bg-theme-neutral-40 text-theme-neutral-60" type="text" placeholder="請輸入密碼">
+                        </label>
+                        <label class="flex justify-between gap-2 font-bold text-base leading-[1.5] tracking-wide w-full" for="">
+                            <span>
+                                <input class="" type="checkbox">
+                                記住帳號
+                            </span>
+                            <a class=" justify-self-end-safe text-theme-primary-100 underline" href="">忘記密碼？</a>
+                        </label>
+                    </div>
+                    <button class="w-full rounded-md p-4 bg-theme-neutral-40 text-theme-neutral-60">會員登入</button>
+                    <h4>沒有會員嗎？
+                        <a  class=" text-theme-primary-100 underline" href="">前往註冊</a>
                     </h4>
                 </form>
             </div>
