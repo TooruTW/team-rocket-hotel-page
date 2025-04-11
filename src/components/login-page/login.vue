e04290d25fdcf51d26de593c2e6a0af7a9e102da
Tooru Wu <tooruwu@ToorudeMacBook-Air.local>
2025-04-11 10:10:07 +0800

    feat-.vue.file

---
+++ src/components/login-page/login.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+</script>
+<template>
+    <div>
+        <nav></nav>
+        <div>
+            <img src="" alt="login-img">
+            <div>
+                <img src="" alt="decroation-wave">
+                <form action="">
+                    <div>
+                        <p></p>
+                        <h1></h1>
+                    </div>
+                    <label for="">
+                        <h5></h5>
+                        <input type="text">
+                    </label>
+                    <label for="">
+                        <h5></h5>
+                        <input type="text">
+                    </label>
+                    <label for="">
+                        <input type="checkbox">
+                        <a href=""></a>
+                    </label>
+                    <button></button>
+                    <h4>
+                        <a href=""></a>
+                    </h4>
+                </form>
+            </div>
+        </div>
+    </div>
+</template>
