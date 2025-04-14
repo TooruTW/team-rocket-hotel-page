fd19e58f0f794e55bcc40061393a7b3548ade852
Tooru Wu <tooruwu@ToorudeMacBook-Air.local>
2025-04-14 15:39:33 +0800

    feat-register-local-validator

--- src/components/user-page/RegisterForm.vue
+++ src/components/user-page/RegisterForm.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { ref, computed } from 'vue'
 import cityName from '../asset/CityCountyData.json'
-import { isEmail, isPassword } from '../../validator'
-
+import { isEmail, isPassword, isPhoneNum, isBirthday, isAddress } from '../../validator'
 const isStage2 = ref(false)
+const isStage1Pass = ref(false)
+const isStage2Pass = ref(false)
+const today = new Date()
 const stageClass = computed(()=> {
     if(!isStage2.value){
         return "border-1  border-theme-neutral-60 text-theme-neutral-60"
@@ -11,7 +13,6 @@ const stageClass = computed(()=> {
     return "border-0 bg-theme-primary-100 text-theme-neutral-0"
 })
 // stage-1
-
 const user = ref(
     {
         name: null,
@@ -22,54 +23,130 @@ const user = ref(
         address: {
             zipcode: null,
             detail: null
-        }
+        },
+        isReaded: false
     }
 )
-
 const userPasswordConfirm = ref(null)
-
 const validatorEmail = ref(null)
 const validatorPassword = ref(null)
 const validatorPasswordConfirem = ref(null)
-const stage1Validator = computed(()=>{
-    return validatorEmail.value || validatorPassword.value || validatorPasswordConfirem.value ? false:true;
-})
+
+const validatorName = ref(null)
+const validatorPhone = ref(null)
+const validatorBirthday = ref(null)
+const validatorAddress = ref(null)
+const validatorAggrement = ref(null)
+
+function stage1Checker(){
+        // stage 1
+        validatorEmail.value = isEmail(user.value.email)? null:"電子郵件格式有誤。"
+        validatorPassword.value = isPassword(user.value.password)? null:"請輸入 8 碼密碼，且需包含至少一個英文字。"
+        validatorPasswordConfirem.value = user.value.password === userPasswordConfirm.value? null : "與輸入密碼不符"
+        if(!validatorEmail.value && !validatorPassword.value && !validatorPasswordConfirem.value){
+            isStage1Pass.value = true
+            console.log("Stage 1 pass")
+        } 
+
+}
+
+function stage2Checker(){
+    // stage 2
+    if(isStage2){
+            validatorName.value = user.value.name? null:"Hello Miss./Mr. Unknow."
+            validatorPhone.value = isPhoneNum(user.value.phone)? null:"手機號碼格式有誤。"
+            validatorBirthday.value = isBirthday(user.value.birthday)? null:"這麼小就想訂房間？？？"
+            validatorAddress.value = isAddress(user.value.address.detail)? null:"你住這？"
+            validatorAggrement.value = user.value.isReaded? null:"我知道你不會讀 但還是要勾。"
+        }
+        if( !validatorEmail.value && !validatorPassword.value && !validatorPasswordConfirem.value && !validatorName.value && !validatorPhone.value && !validatorBirthday.value && !validatorAddress.value && !validatorAggrement.value){
+            console.log("stage 2 pass")
+            isStage2Pass.value = true
+        }
+}
 
 function formChecker(){
-    console.log(user.value)
-    validatorEmail.value = isEmail(user.value.email)? null:"電子郵件格式錯誤。"
-    validatorPassword.value = isPassword(user.value.password)? null:"請輸入 8 碼密碼，且需包含至少一個英文字。"
-    validatorPasswordConfirem.value = user.value.password === userPasswordConfirm.value? null : "與輸入密碼不符"
-}
 
-function handleRegiserAccount(event){
+    // stage 1
+
+        validatorEmail.value = isEmail(user.value.email)? null:"電子郵件格式有誤。"
+        validatorPassword.value = isPassword(user.value.password)? null:"請輸入 8 碼密碼，且需包含至少一個英文字。"
+        validatorPasswordConfirem.value = user.value.password === userPasswordConfirm.value? null : "與輸入密碼不符"
+        if(!validatorEmail.value && !validatorPassword.value && !validatorPasswordConfirem.value){
+            isStage1Pass.value = true
+        } else return
+
+    // stage 2
+       
+        if(isStage2){
+            validatorName.value = user.value.name? null:"Hello Miss./Mr. Unknow."
+            validatorPhone.value = isPhoneNum(user.value.phone)? null:"手機號碼格式有誤。"
+            validatorBirthday.value = isBirthday(user.value.birthday)? null:"這麼小就想訂房間？？？"
+            validatorAddress.value = isAddress(user.value.address.detail)? null:"你住這？"
+            validatorAggrement.value = user.value.isReaded? null:"我知道你不會讀 但還是要勾。"
+        }
+        if( !validatorEmail.value && !validatorPassword.value && !validatorPasswordConfirem.value && !validatorName.value && !validatorPhone.value && !validatorBirthday.value && !validatorAddress.value && !validatorAggrement.value){
+            console.log("stage 2 pass")
+            isStage2Pass.value = true
+        }
+    }
+
+function handleRegiserNext(event){
     event.preventDefault()
-    console.log(stage1Validator.value)
-    stage1Validator.value? isStage2.value = true : null ;
-}
+    stage1Checker()
 
+    isStage1Pass.value? isStage2.value = true : false
+}
 
 // stage-2
+
+function formatorBirthday(){
+    user.value.birthday = `${selectedYear.value}/${selectedMonth.value}/${selectedDay.value}`
+}
+
 const cityArr = computed(()=>{
     return cityName.map((city)=> city.CityName)
 })
 const selectedCity = ref("高雄市")
 const selectedZone = ref('新興區')
+const selectedZipCode = computed(()=>{
+    const zonesInfo = cityName.find((city)=> city.CityName === selectedCity.value ).AreaList
+    const zipCode = zonesInfo.find(zone => zone.AreaName === selectedZone.value).ZipCode
+    return zipCode
+})
+
 const zoneArr = computed(()=>{
     if(!selectedCity.value) return []
     const zonesInfo = cityName.find((city)=> city.CityName === selectedCity.value ).AreaList
-    const zoneName = zonesInfo.map(zone => zone.AreaName)
+    const zoneName = zonesInfo.map(zone => zone.AreaName )
     return zoneName 
 })
-const years = Array.from({ length: 100 }, (_, i) => 2025 - i)
+const years = Array.from({ length: 100 }, (_, i) => today.getFullYear() - i)
 const months = Array.from({ length: 12 }, (_, i) => i + 1)
-const selectedYear = ref(new Date().getFullYear())
-const selectedMonth = ref(new Date().getMonth() + 1)
-const selectedDay = ref(new Date().getDate())
+const selectedYear = ref(today.getFullYear())
+const selectedMonth = ref(today.getMonth() + 1)
+const selectedDay = ref(today.getDate())
 const days = computed(() => {
   const lastDay = new Date(selectedYear.value, selectedMonth.value, 0).getDate()
   return Array.from({ length: lastDay }, (_, i) => i + 1)
 })
+
+function updateZipCode(){
+    user.value.address.zipcode = selectedZipCode.value
+}
+
+function handleSubmit(event){
+    event.preventDefault()
+    updateZipCode()
+    formatorBirthday()
+    stage2Checker()
+
+    if(isStage1Pass.value && isStage2Pass.value){
+        console.log("Register Compeleted sending data to sever")
+        return
+    }
+    console.log("Validate fail",user.value)
+}
 
 </script>
 <template>
@@ -96,60 +173,60 @@ const days = computed(() => {
                             <p class="flex gap-2 items-center">
                                 電子信箱 <span class="text-14 text-theme-alert-100">  {{ validatorEmail }} </span>
                             </p>
-                            <input @input="formChecker" v-model="user.email" id="email" class="w-full rounded-md p-4 bg-theme-neutral-40 text-theme-neutral-60 font-medium" type="text" placeholder="hello@exsample.com">
+                            <input v-model="user.email" id="email" class="w-full rounded-md p-4 bg-theme-neutral-40 text-theme-neutral-60 font-medium" type="text" placeholder="hello@exsample.com">
                         </label>
                         <label class=" font-bold text-base max-md:text-14 leading-[1.5] tracking-wide w-full flex flex-col gap-2" for="">
                             <p class="flex gap-2 items-center">
                                 密碼 <span class="text-14 text-theme-alert-100">  {{ validatorPassword }} </span>
                             </p>
-                            <input @input="formChecker" v-model="user.password" class="w-full rounded-md p-4 bg-theme-neutral-40 text-theme-neutral-60 font-medium" type="text" placeholder="請輸入密碼">
+                            <input v-model="user.password" class="w-full rounded-md p-4 bg-theme-neutral-40 text-theme-neutral-60 font-medium" type="text" placeholder="請輸入密碼">
                         </label>
                         <label class=" font-bold text-base max-md:text-14 leading-[1.5] tracking-wide w-full flex flex-col gap-2" for="">
                             <p class="flex gap-2 items-center">
                                 確認密碼 <span class="text-14 text-theme-alert-100">  {{ validatorPasswordConfirem }} </span>
                             </p>
-                            <input @input="formChecker" v-model="userPasswordConfirm" class="w-full rounded-md p-4 bg-theme-neutral-40 text-theme-neutral-60 font-medium" type="text" placeholder="請再輸入一次密碼">
+                            <input v-model="userPasswordConfirm" class="w-full rounded-md p-4 bg-theme-neutral-40 text-theme-neutral-60 font-medium" type="text" placeholder="請再輸入一次密碼">
                         </label>
-                        <button @click="handleRegiserAccount" class="w-full mt-10 rounded-md p-4 bg-theme-neutral-40 text-theme-neutral-60 hover:bg-theme-primary-100 hover:text-theme-neutral-0">下一步</button>
+                        <button @click="handleRegiserNext" class="w-full mt-10 rounded-md p-4 bg-theme-neutral-40 text-theme-neutral-60 hover:bg-theme-primary-100 hover:text-theme-neutral-0">下一步</button>
                     </div>
                     <!-- stage 2 -->
                     <div v-if="isStage2" class="flex flex-col gap-2">
                         <label class=" font-bold text-base max-md:text-14 leading-[1.5] tracking-wide w-full flex flex-col gap-2" for="fullName">
-                            姓名
-                            <input id="fullName" class="w-full rounded-md p-4 bg-theme-neutral-40 text-theme-neutral-60 font-medium" type="text" placeholder="請輸入姓名">
+                            <p class="flex items-center gap-2">姓名 <span class="text-14 text-theme-alert-100">{{ validatorName }}</span></p>
+                            <input v-model="user.name" id="fullName" class="w-full rounded-md p-4 bg-theme-neutral-40 text-theme-neutral-60 font-medium" type="text" placeholder="請輸入姓名">
                         </label>
                         <label class=" font-bold text-base max-md:text-14 leading-[1.5] tracking-wide w-full flex flex-col gap-2" for="phoneNum">
-                            手機號碼
-                            <input id="phoneNum" class="w-full rounded-md p-4 bg-theme-neutral-40 text-theme-neutral-60 font-medium" type="text" placeholder="請輸入手機號碼">
+                            <p class="flex items-center gap-2">手機號碼 <span class="text-14 text-theme-alert-100">{{ validatorPhone }}</span></p>
+                            <input v-model="user.phone" id="phoneNum" class="w-full rounded-md p-4 bg-theme-neutral-40 text-theme-neutral-60 font-medium" type="text" placeholder="請輸入手機號碼">
                         </label>
                         <label class=" font-bold text-base max-md:text-14 leading-[1.5] tracking-wide" for="year">
-                            生日
+                            <p class="flex items-center gap-2">生日 <span class="text-14 text-theme-alert-100">{{ validatorBirthday }}</span></p>
                             <div class="flex font-medium justify-between gap-2 text-theme-neutral-80">
                                 <!-- year -->
                                 <div class="w-full flex items-center relative bg-theme-neutral-0 rounded-md">
-                                    <select class="appearance-none p-4 w-full" name="year" id="year" v-model="selectedYear">
-                                        <option v-for="year in years" :key="year" :value="year">{{ year }}年</option>
+                                    <select @change="formatorBirthday" class="appearance-none p-4 w-full" name="year" id="year" v-model="selectedYear">
+                                        <option v-for="year in years" :key="year" :value="year">{{ year }}</option>
                                     </select>
                                     <svg class="pointer-events-none absolute right-4" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z"/></svg>
                                 </div>
                                 <!-- month -->
                                 <div class="w-full flex items-center relative bg-theme-neutral-0 rounded-md">
-                                    <select class="appearance-none p-4 w-full" name="month" id="month" v-model="selectedMonth">
-                                        <option v-for="month in months" :key="month" :value="month">{{ month }}月</option>
+                                    <select @change="formatorBirthday" class="appearance-none p-4 w-full" name="month" id="month" v-model="selectedMonth">
+                                        <option v-for="month in months" :key="month" :value="month">{{ month }}</option>
                                     </select>
                                     <svg class="pointer-events-none absolute right-4" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z"/></svg>
                                 </div>
                                 <!-- day -->
                                 <div class="w-full flex items-center relative bg-theme-neutral-0 rounded-md">
-                                    <select class="appearance-none p-4 w-full" name="day" id="day" v-model="selectedDay">
-                                        <option v-for="day in days" :key="day" :value="day">{{ day }}日</option>
+                                    <select @change="formatorBirthday" class="appearance-none p-4 w-full" name="day" id="day" v-model="selectedDay">
+                                        <option v-for="day in days" :key="day" :value="day">{{ day }}</option>
                                     </select>
                                     <svg class="pointer-events-none absolute right-4" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z"/></svg>
                                 </div>
                             </div>
                         </label>
                         <label class=" font-bold text-base max-md:text-14 leading-[1.5] tracking-wide w-full flex flex-col gap-2" for="">
-                            地址
+                            <p class="flex items-center gap-2">地址 <span class="text-14 text-theme-alert-100">{{ validatorAddress }}</span></p>
                             <div class="flex gap-2 text-theme-neutral-80">
                                 <div class="w-full flex items-center relative bg-theme-neutral-0 rounded-md">
                                     <select class="appearance-none p-4 w-full" name="city" id="city" v-model="selectedCity">
@@ -164,13 +241,15 @@ const days = computed(() => {
                                     <svg class="pointer-events-none absolute right-4" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z"/></svg>
                                 </div>
                             </div>
-                            <input class="w-full rounded-md p-4 bg-theme-neutral-40 text-theme-neutral-60 font-medium" type="text" placeholder="請輸入詳細地址">
+                            <input v-model="user.address.detail" class="w-full rounded-md p-4 bg-theme-neutral-40 text-theme-neutral-60 font-medium" type="text" placeholder="請輸入詳細地址">
                         </label>
                         <label for="reade" class="flex gap-2">
-                            <input id="readed" type="checkbox">
-                            <p>我已閱讀並同意本網站個資使用規範</p>
+                            <input v-model="user.isReaded" id="readed" type="checkbox">
+                            <p class="flex items-center gap-2">我已閱讀並同意本網站個資使用規範 
+                                <span class="text-14 text-theme-alert-100">{{ validatorAggrement }}</span>
+                            </p>
                         </label>
-                        <button class="w-full mt-10 rounded-md p-4  bg-theme-neutral-40 text-theme-neutral-60 hover:bg-theme-primary-100 hover:text-theme-neutral-0">完成註冊</button>
+                        <button @click="handleSubmit" class="w-full mt-10 rounded-md p-4  bg-theme-neutral-40 text-theme-neutral-60 hover:bg-theme-primary-100 hover:text-theme-neutral-0">完成註冊</button>
                     </div>
                     <h4 class="font-medium text-base max-md:text-14 leading-[1.5] tracking-wide">已經有會員了嗎？
                         <a  class="font-bold text-theme-primary-100 underline" href="">立即登入</a>
--- src/validator.js
+++ src/validator.js
@@ -8,3 +8,25 @@ export function isPassword(str){
     const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
     return passwordRegex.test(str)
 }
+
+export function isPhoneNum(str){
+    const phoneRegex = /^09\d{8}$/
+    return phoneRegex.test(str)
+}
+
+export function isBirthday(str){
+    const birthdayRegex = /^(19\d{2}|20\d{2})\/([1-9]|1[0-2])\/(0[1-9]|[12]\d|3[01])$/
+    if(!birthdayRegex.test(str)){
+        console.log("生日格式錯誤")
+        return false
+    }
+    const age = str.split("/")
+    const thisYear = new Date().getFullYear()
+    return ((thisYear - Number(age[0])) > 17 )
+}
+
+export function isAddress(str) {
+    const cleaned = str.replace(/　/g, ' ') // 把全形空格換半形
+    const addressRegex = /^[\u4e00-\u9fa5a-zA-Z0-9\s]+?\d+\s*號.*$/
+    return addressRegex.test(cleaned)
+}
