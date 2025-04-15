97eec534a5b9cad72ae6215f5bb58a5dea3b74c6
Tooru Wu <tooruwu@ToorudeMacBook-Air.local>
2025-04-15 16:53:09 +0800

    feat-basic-structure

---
+++ src/components/rooms-page/RoomsPage.vue
@@ -0,0 +1,25 @@
+<section setup>
+
+</section>
+<template>
+    <!-- hero -->
+     <div>
+        <!-- header -->
+        <!-- title -->
+     </div>
+    <!-- content -->
+     <div>
+        <!-- title -->
+         <div>
+            <p></p>
+            <h1></h1>
+         </div>
+        <!-- card container -->
+         <div>
+            <!-- cards -->
+            <div></div>
+         </div>
+     </div>
+     <!-- footer -->
+
+</template>
