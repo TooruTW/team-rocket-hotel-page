3e79361bf148a9a46e645f382fad3e7b96a3c904
Tooru Wu <tooruwu@ToorudeMacBook-Air.local>
2025-04-16 11:10:49 +0800

    feat-card-templete

--- src/components/rooms-page/RoomsPage.vue
+++ src/components/rooms-page/RoomsPage.vue
@@ -2,24 +2,14 @@
     import {ref} from 'vue'
     import Footer from "../Footer.vue"
     import Hero from './Hero.vue'
+    import RoomList from './RoomList.vue'
 </script>
 <template>
     <div class="flex flex-col w-full min-h-screen">
         <!-- hero -->
             <Hero></Hero>
         <!-- content -->
-        <div>
-            <!-- title -->
-            <div>
-                <p></p>
-                <h1></h1>
-            </div>
-            <!-- card container -->
-            <div>
-                <!-- cards -->
-                <div></div>
-            </div>
-        </div>
+            <RoomList></RoomList>
         <!-- footer -->
         <div class="flex justify-center items-center bg-theme-neutral-bg max-md:h-screen">
             <Footer></Footer>
