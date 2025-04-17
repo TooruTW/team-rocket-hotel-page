d06a7608ccf3fdcbf38ef318698e95552cc098aa
Tooru Wu <tooruwu@ToorudeMacBook-Air.local>
2025-04-17 09:54:46 +0800

    feat-header-footer

--- src/components/rooms-page/RoomDetail/RoomDetail.vue
+++ src/components/rooms-page/RoomDetail/RoomDetail.vue
@@ -1,5 +1,19 @@
 <script setup>
+import Header from '../../Header.vue';
+import Footer from '../../Footer.vue';
 </script>
 
 <template>
+  <div>
+    <!-- header -->
+    <div class="w-full bg-theme-neutral-bg flex justify-center px-20">
+        <Header></Header>
+    </div>
+    <!-- content -->
+    <div></div>
+    <!-- footer -->
+    <div class="w-full bg-theme-neutral-bg flex justify-center px-20">
+        <Footer></Footer>
+    </div>
+  </div>
 </template>
